7b2f422d4958bdbac8f61c45ce1b39e9d0b2076f
prnvvv <prannavakhanth12@gmail.com>
2024-06-10 20:47:54 +0530

    Update FaceDetector_Project.py

--- FaceDetector_Project.py
+++ FaceDetector_Project.py
@@ -8,9 +8,15 @@ if not capture.isOpened:
     print("Webcam Cap is on.")
     exit()
 
+currentTime = 0
+previousTime = 0
+
 while True:
-    success, vidObject = cv2.read()
+    success, vidObject = capture.read()
 
     if not success:
         print("Video Capture failed.")
-        
+
+    currentTime = time.time()
+    fps = 1 / (currentTime - previousTime)
+    previousTime = currentTime
